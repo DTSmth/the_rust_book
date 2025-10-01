7010ffc8a514151a9fd048ad2f16af3654b4f221
DTSmth <ducktapedave85@gmail.com>
2025-10-01 18:21:49 -0400

    learning enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    enum IpAddr {
+        V4 (String),
+        V6 (String),
+    }
+
+
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+
 }
