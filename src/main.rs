6853b0f8e6f2083f6f86f7952f03c897e34dac32
DTSmth <ducktapedave85@gmail.com>
2025-10-01 19:07:26 -0400

    ore enum stuff

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,19 @@ fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopback = IpAddr::V6(String::from("::1"));
 
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32},
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
 
+    impl Message{
+        fn call(&self){
+            // do stuff
+        }
+    }
+
+    let m = Message::Write(String::from("Hello, World!"));
+    m.call();
 }
