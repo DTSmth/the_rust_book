55f0092dbd123dfe7c9063cf9f61774cdb34d668
DTSmth <ducktapedave85@gmail.com>
2025-10-02 13:50:39 -0400

    6.2 more rust enums and match control flow

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,43 @@ fn main() {
 
     let m = Message::Write(String::from("Hello, World!"));
     m.call();
+
+    #[derive(Debug)]
+    enum UsState {
+        NorthCarolina,
+        Virgina,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {state:?}!");
+                25
+            },
+        }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
