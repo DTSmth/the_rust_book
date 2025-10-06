6fe5524990f2431c15b8c02890e1a7ec94f1360c
DTSmth <ducktapedave85@gmail.com>
2025-10-06 15:56:24 -0400

    8.1 Storing Lists of Values with Vectors

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,37 @@ fn main() {
         }
     }
 
+    let v: Vec<i32> = Vec::new();
+
+    let v = vec![1,2,3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    let third = &v[2];
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third elemens is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![1,2,3];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec! [
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
 
 
 }
