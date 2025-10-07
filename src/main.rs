2870d4f278eb1057490fc1b5f8973dc0bf1489ad
DTSmth <ducktapedave85@gmail.com>
2025-10-07 18:41:29 -0400

    8.2 Storing UTF-8 Encoded Text with Strings

--- src/main.rs
+++ src/main.rs
@@ -134,6 +134,38 @@ fn main() {
         SpreadsheetCell::Float(10.12),
     ];
 
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("inital contents");
 
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("Foo");
+    let s2 = "Bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("hell0, ");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    
 
 }
