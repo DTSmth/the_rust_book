1cc7528f633562ef8ddf0bdef6e5cbf5c3d1f96e
DTSmth <ducktapedave85@gmail.com>
2025-10-10 13:10:27 -0400

    8.3 Storing Keys with Associated Values in Hash Maps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     enum IpAddr {
         V4 (String),
@@ -166,6 +168,47 @@ fn main() {
     let s3 = String::from("toe");
 
     let s = format!("{s1}-{s2}-{s3}");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 59);
+    println!("scores = {:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(100);
+    scores.entry(String::from("Blue")).or_insert(200);
+
+    println!("scores = {:?}", scores);
+
+    let text = "yellow world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("map = {:?}", map);
+
+
     
 
 }
